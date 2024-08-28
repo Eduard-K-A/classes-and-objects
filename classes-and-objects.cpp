c1ace2fbd15726d277218ee81183f4ab140e8cef
Eduard King Anterola <eduard.king.anterola@dlsl.edu.ph>
2024-08-28 21:00:26 +0800

    second commit

--- classes-and-objects.cpp
+++ classes-and-objects.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+int bookCounter = 0;
 class Library
 {
 private:
@@ -12,16 +13,26 @@ private:
 
 public:
     void addBook(){ //asks the user to input author name
+
+        cout << "Enter Title name: ";
+        cin.ignore();   
+        getline(cin, title);
+
         cout << "Enter author name: ";
         cin.ignore();   
         getline(cin, author);
-        cout << "Added succesfully" << endl;
+
+        cout << "Enter Publication Year: ";
+        cin >> publishDate;
+
+        cout << "Book Added Succesfully!" << endl;
+        bookCounter++;
+       // cout << bookCounter << endl;
     }
 
     void displayBook(){ //displays book details
-        cout << "Author: " << author << endl;
-        cout << "Title: " << title << endl;
-        cout << "Publish Date: " << publishDate << endl;
+    cout << "Title" << setw(20) << "Author" << setw(20) << "Year" << endl;
+    cout << title << setw(20) << author << setw(20) << publishDate << endl;
         }
 
     void searchBook(){ //search book through title
@@ -34,27 +45,43 @@ public:
 int main()
 {
     int choice;
+    char addMoreBook;
+    bool exit = true;
+    Library s;
+
 
     cout << "Menu" << endl; //menu
     cout << "1. Add Book" << endl;
     cout << "2. Display Book" << endl;
     cout << "3. Search Book" << endl;
     cout << "4. Exit" << endl;
+
+    cout << "Enter a number: ";
     cin >> choice;
 
+    cin.ignore();
+    do{
     switch (choice)
     {
     case 1:
-        // add book
+    do{
+        s.addBook();
+        cout << "Would you like to add more? (Y/N): ";
+        cin >> addMoreBook;
+    }while(addMoreBook == 'y' || addMoreBook == 'Y');
+
+        break;
     case 2:
-        // display book
+        s.displayBook();
+        break;
     case 3:
         // search book
+        break;
     case 4:
-        // exit
-    default:
-        // exit
+        exit = false;
     }
-    cout << "hello world!!!" << endl;
+    } while(exit == true);
+    cout << "done" << endl;
+   
     return 0;
 }
